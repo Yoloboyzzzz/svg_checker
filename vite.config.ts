e72895c0fbb61f5bde5730daa2cc287fed509fb6
Henry Clasen <henry.clasen@gmail.com>
2026-03-25 13:46:18 +0100

    prepare for github pages

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
+  base: '/svg_checker/',
   plugins: [react(), tailwindcss()],
   test: {
     environment: 'jsdom',
